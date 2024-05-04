dac20601f2f240d8fae259c108c0c0d30a048836
Krishna Sonawane <studysppu@gmail.com>
2024-05-04 16:16:18 +0530

    Adding Files

---
+++ hi.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+import java.util.List;
+
+public class hi {
+
+    int x,y,z;
+
+    
+
+    hi(){
+          
+    }
+
+    public static void main(String[] args) {
+
+        List list;
+
+        BigData data = new BigData();
+
+
+        
+
+    }
+
+    
+}
